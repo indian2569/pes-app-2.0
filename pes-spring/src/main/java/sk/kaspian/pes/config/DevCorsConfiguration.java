4f4103660d2d63e567cce5f66aea85d053b4feea
Viliam Litavec <indian2569@centrum.sk>
2023-12-11 01:56:37 +0100

    reset branch + cors config

--- pes-spring/src/main/java/sk/kaspian/pes/config/DevCorsConfiguration.java
+++ pes-spring/src/main/java/sk/kaspian/pes/config/DevCorsConfiguration.java
@@ -6,7 +6,6 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@Profile("development")
 public class DevCorsConfiguration implements WebMvcConfigurer {
 
     @Override
