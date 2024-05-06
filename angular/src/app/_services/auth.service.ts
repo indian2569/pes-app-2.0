4f60dd51805b54a6767265c1a82b19c592c79850
Viliam Litavec <indian2569@centrum.sk>
2024-05-06 13:40:30 +0200

    Fix QA code clean

--- angular/src/app/_services/auth.service.ts
+++ angular/src/app/_services/auth.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { ConfigService } from './config.service';
-import { take } from 'rxjs/operators';
 
 let AUTH_API = 'https://kaspian-pes.online/api/auth/';
 
